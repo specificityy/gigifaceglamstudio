85c298217a3cbab0f32f0e5df8395f62ffab6368
Wilson Jimenez <wilsonhjimenez@gmail.com>
2020-06-07 10:18:47 +0100

    very wip

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -7,6 +7,8 @@ import { ThemeProvider } from 'emotion-theming';
 import { jsx, css, keyframes } from '@emotion/core';
 import { v4 as uuidv4 } from 'uuid';
 import clamp from 'lodash/clamp';
+import debounce from 'lodash/debounce';
+import { BehaviorSubject } from 'rxjs';
 
 import { PreviewCompatibleBackgroundImage } from '../components/PreviewCompatibleBackgroundImage';
 import theme from '../components/theme';
@@ -15,43 +17,96 @@ import InstagramIcon from '../img/social/instagram.svg';
 import YouTubeIcon from '../img/social/youtube.svg';
 import MirrorSvg from '../img/dressing-room-mirror.svg';
 
-const thresholdArray = Array.from(Array(20).keys(), i => i / 20);
+const thresholdArray = [0.4]; //Array.from(Array(10).keys(), i => i / 10);
+
+const activeSlide$ = new BehaviorSubject(5);
+let _active = 1000;
+let _pictures;
+
+const BASE_ZINDEX = 10;
+
+const render = () => {
+    for (let i = 0; i < _pictures.length; i++) {
+        const pic = _pictures[i];
+        const bgImage = pic.querySelector('.bg-image');
+
+        if (i === _active) {
+            pic.style.marginLeft = '-8%';
+            bgImage.style.transform = 'rotateY(0)';
+            pic.style.zIndex = BASE_ZINDEX + _pictures.length;
+        } else if (i < _active) {
+            pic.style.marginLeft = '-8%';
+            bgImage.style.transform = 'rotateY(130deg) scaleX(0.5)';
+            bgImage.style.transformOrigin = 'left';
+            pic.style.zIndex = BASE_ZINDEX + i;
+        } else {
+            pic.style.marginLeft = '-10%';
+            bgImage.style.transform = 'rotateY(-9deg)';
+            bgImage.style.transformOrigin = 'right';
+            pic.style.zIndex = BASE_ZINDEX + (_pictures.length - i);
+        }
+    }
+};
 
 export const HomePageTemplate = ({ heading, description, pictures }) => {
     // console.log(pictures);
     const content = useRef(null);
-
-    const [pos, setPos] = useState('');
+    const slideShow = useRef(null);
+    const [active, setActive] = useState(5);
 
     useEffect(() => {
-        window.addEventListener('mousewheel', mouseWheelHandler);
+        // window.addEventListener('mousewheel', mouseWheelHandler);
 
-        function mouseWheelHandler(e) {
-            if (content.current === undefined) {
-                content.current = window.document.body;
-            } else {
-                content.current.scrollLeft += e.deltaY;
-            }
-        }
+        // function mouseWheelHandler(e) {
+        //     if (content.current === undefined) {
+        //         content.current = window.document.body;
+        //     } else {
+        //         content.current.scrollLeft += e.deltaY;
+        //     }
+        // }
+
+        _pictures = Array.prototype.slice.call(window.document.querySelectorAll('.picture'));
+        // _index = Math.floor(_pictures.length / 2);
+
+        // // render();
+
+        // console.log(_pictures);
+
+        // window.setTimeout(() => {
+        //     content.current.addEventListener('scroll', handleScroll);
+        //     console.log(content.current);
+        // });
+
+        // const handleScroll = debounce(e => {
+        //     console.log(e);
+        // }, 300);
+
+        activeSlide$.subscribe(active => {
+            console.log(active);
+            _active = active;
+            render();
+        });
 
         return () => {
-            window.removeEventListener('mousewheel', mouseWheelHandler);
+            // window.removeEventListener('mousewheel', mouseWheelHandler);
+            // content.current.removeEventListener('scroll', handleScroll);
         };
     }, [content]);
 
     return (
         <ThemeProvider theme={theme}>
             <Container ref={content}>
-                <h1 style={{ position: 'fixed', left: '10vw', top: '100px' }}>{pos}</h1>
-                <SlidesShow>
-                    <WhoAmI />
-                    <Mirror />
+                {/* <Mirror /> */}
 
-                    {pictures.map(pic => (
-                        <Picture key={uuidv4()} image={pic.image} caption={pic.caption} />
-                    ))}
+                <SlideShow ref={slideShow}>
                     <WhoAmI />
-                </SlidesShow>
+
+                    {pictures.map((pic, index) => (
+                        <Picture key={uuidv4()} image={pic.image} caption={pic.caption} index={index} />
+                    ))}
+
+                    <WhoAmI />
+                </SlideShow>
             </Container>
         </ThemeProvider>
     );
@@ -75,52 +130,79 @@ const Mirror = styled(MirrorSvg)`
     }
 `;
 
-const Picture = ({ image, caption }) => {
+const Picture = ({ image, caption, index }) => {
+    const imageRef = useRef(null);
+
     const [ref, entry] = useIntersection({
         rootMargin: '0px',
         threshold: thresholdArray,
     });
 
     const { intersectionRatio = 1 } = entry || {};
-    const scale = clamp(intersectionRatio > 0.8 ? 1 : intersectionRatio, 0.9, 1);
+    const scale = clamp(intersectionRatio > 0.8 ? 1 : intersectionRatio, 0.5, 1);
     const opacity = clamp(intersectionRatio > 0.6 ? 1 : intersectionRatio, 0.3, 1);
 
-    console.log(caption, scale, intersectionRatio);
+    const visible = intersectionRatio > 0.4;
+
+    console.log({ caption, intersectionRatio });
+
+    if (visible) {
+        activeSlide$.next(index);
+    }
 
     return (
-        <>
-            <ImageWrapper ref={ref} scale={scale} opacity={scale === 1 ? 0 : 0.5}>
-                <BackgroundImage imageInfo={image} />
-                <h3>{caption}</h3>
-            </ImageWrapper>
-            {scale === 1 ? (
-                <DisplayedImage>
-                    <BackgroundImage imageInfo={image} />
-                    <h3>{caption}</h3>
-                </DisplayedImage>
-            ) : null}
-        </>
+        <ImageWrapper ref={ref} scale={0.8} active={_active === index} before={index < _active} className="picture">
+            <BackgroundImage imageInfo={image} className="bg-image" />
+            <h3>{caption}</h3>
+        </ImageWrapper>
     );
 };
 
-const DisplayedImage = styled.div`
-    position: fixed;
-    z-index: 10;
-    left: 50%;
-    transform: translateX(-50%);
-`;
-
 const ImageWrapper = styled.div`
+    perspective: 600px;
     transition: transform 300ms, opacity 1000ms;
     transform: ${props => `scale(${props.scale})`};
-    opacity: ${props => `${props.opacity}`};
-    will-change: transform;
+
+    scroll-snap-align: start;
+
+    ${props => {
+        // if (props.active)
+        //     return `
+        //     margin-left: -8%;
+        // `;
+        // if (props.before) return 'margin-left: -8%;';
+        // return `
+        //     margin-left: -10%;
+        // `;
+    }}
+
+    & > * {
+        transform-style: preserve-3d;
+        will-change: transform;
+        transition: transform 500ms;
+
+        // -webkit-box-reflect: below 0;
+        // -webkit-gradient(linear, left top, left bottom, from(transparent), color-stop(0.3, transparent), to(white));
+
+        ${props => {
+            // if (props.active) return `transform: rotateY(0);`;
+            // if (props.before)
+            //     return `
+            //         transform: rotateY(130deg) scaleX(0.5);
+            //         transform-origin: left;
+            //     `;
+            // return `
+            //     transform: rotateY(-9deg);
+            //     transform-origin: right;
+            // `;
+        }}
+    }
 `;
 
 const BackgroundImage = styled(PreviewCompatibleBackgroundImage)`
     ${props => `
-    width: 50vw;
-    height: 50vh;
+    width: 40vw;
+    height: 40vh;
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
@@ -142,16 +224,22 @@ const Container = styled.main`
 
     color: wheat;
     background-color: #1d1d1d;
+
+    scroll-snap-type: x mandatory;
 `;
 
-const SlidesShow = styled.div`
+const SlideShow = styled.div`
     display: flex;
     align-items: center;
     margin: 0 100px;
     padding-right: 2vw;
     & > * {
         flex: 1 0 auto;
-        margin-right: 100px;
+        margin-right: -25%;
+
+        &:nth-of-type(2) {
+            // margin-left: -35%;
+        }
     }
 `;
 
